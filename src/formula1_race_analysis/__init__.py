1174c0dd36ad94ee75b366f6ccf9a6ee9934eac2
KrisKulbych <k.kulbych@gmail.com>
2025-03-04 09:22:52 +0200

    integrated pydantic in project

--- src/formula1_race_analysis/__init__.py
+++ src/formula1_race_analysis/__init__.py
@@ -1,18 +1,14 @@
-from .custom_exceptions import (
+from .custom_types import LapTimeDict, TimeStampDict
+from .exceptions import (
     InvalidFormatDataError,
+    InvalidNameFormatError,
     InvalidRaceTimeError,
     MissedFileError,
 )
-from .data_formatter import (
-    convert_timestamp_to_seconds,
-    format_data_from_abbreviation_file,
-    format_data_from_log_file,
-    format_driver_name,
-    format_lap_time,
-)
-from .driver_model import Driver
+from .models import Driver, RaceResults
 from .q1_session_analyzer import (
     build_q1_report,
-    parse_abbreviation_file,
+    create_driver_list,
     read_file_content,
 )
+from .schemas import AbbreviationEntry, LogEntry
